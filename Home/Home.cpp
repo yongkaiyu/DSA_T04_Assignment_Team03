daf771f25480c1669c446136e0a05579521e866f
Daniel <s10258472@connect.np.edu.sg>
2026-01-30 10:53:44 +0800

    added removeGame, fixed role menu, fix memberid bug

--- Home/Home.cpp
+++ Home/Home.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 #include "Games.h"
-#include "CSVReader.h" // Include your new utility
+#include "CSVReader.h"
 #include "Admin.h"
 #include "UserDictionary.h"
 #include "User.h"
@@ -13,10 +13,10 @@
 
 using namespace std;
 
-void adminMenu(Admin admin, UserDictionary users, GameDictionary& lib, Booking* bookingSystem) {
+void adminMenu(Admin& admin, UserDictionary& users, GameDictionary& lib, Booking* bookingSystem) {
     int choice = -1;
 
-    while (choice != 0) {
+    while (choice != -2) {
         cout << "\n=== ADMIN MENU ===\n";
         cout << "1) Add a new board game\n";
         cout << "2) Remove a board game\n";
@@ -71,6 +71,20 @@ void adminMenu(Admin admin, UserDictionary users, GameDictionary& lib, Booking* 
         else if (choice == 2) {
             // remove a board game (look at game)
             // need remove game to be added first
+            cout << "Enter game to be removed: ";
+            string gameName;
+            cin.ignore();
+            getline(cin, gameName);
+
+            int copiesLeft = lib.removeGame(gameName);
+
+            if (copiesLeft == -1) {
+                cout << "No such game found.";
+            }
+            else if (copiesLeft == 0) {
+                cout << "Game removed.";
+            }
+        
         }
         else if (choice == 3) {
             // ask for username only (userid not required as its auto generated)
@@ -79,10 +93,12 @@ void adminMenu(Admin admin, UserDictionary users, GameDictionary& lib, Booking* 
             cin >> userName;
 
             // add member
-            bool success = admin.addMember(users, userName);
+            string newID;
+            bool success = admin.addMember(users, userName, newID);
 
             if (success) {
                 cout << "Member created successfully!\n";
+                cout << newID;
             }
             else {
                 cout << "Failed to create member";
@@ -93,7 +109,7 @@ void adminMenu(Admin admin, UserDictionary users, GameDictionary& lib, Booking* 
 			bookingSystem->printAdminSummary();
         }
         else {
-            
+            break;
         }
     }
 }
@@ -138,31 +154,40 @@ int main() {
 	Booking* bookingSystem = new Booking;
 
     UserDictionary users;
-    Admin admin("A001", "Admin");
+    Admin admin("A0001", "Admin");
 
-	int roleChoice = -1;
-	cout << "Select Role:\n1) Admin\n2) Member\nChoose: ";
-	cin >> roleChoice;
-    if (roleChoice == 1)
-    {
-        adminMenu(admin, users, lib, bookingSystem);
-    }
-    else if (roleChoice == 2)
-    {
-        string memberID;
-        cout << "Enter Member ID: ";
-        cin >> memberID;
-        if (users.contains(memberID)) {
-            memberMenu(bookingSystem, memberID);
+    while (true) {
+        int roleChoice = -1;
+        cout << "Select Role:\n1) Admin\n2) Member\nChoose: ";
+        cin >> roleChoice;
+        if (roleChoice == 1)
+        {
+            adminMenu(admin, users, lib, bookingSystem);
         }
-        else {
-            cout << "Member ID not found.\n";
+        else if (roleChoice == 2)
+        {
+            string memberID;
+            cout << "Enter Member ID: ";
+            cin >> memberID;
+            cout << memberID;
+            if (users.contains(memberID)) {
+                memberMenu(bookingSystem, memberID);
+            }
+            else {
+                cout << "Member ID not found.\n";
+                cout << users.getUser(memberID);
+            }
+        }
+        else if (roleChoice == 0) {
+            break;
+        }
+        else
+        {
+            cout << "Invalid role selection.\n";
         }
     }
-    else
-{
-        cout << "Invalid role selection.\n";
-	}
+
+	
 
 	//loadGamesFromCSV("games.csv", lib); // Load data from CSV into the dictionary
 
@@ -170,14 +195,3 @@ int main() {
     
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
